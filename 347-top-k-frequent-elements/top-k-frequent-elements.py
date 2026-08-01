1277a18820b08ecb3796687bb4f9c4b46f015ff8
Mohammad Jaffer Hussin <mohammad.jaffer.hussin@gmail.com>
2026-08-01 23:47:42 +0530

    Time: 4 ms (69.52%) | Memory: 22.9 MB (59.96%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -3,7 +3,7 @@ class Solution:
         count = {}
         for num in nums:
             count[num] = count.get(num , 0) +1
-        sorted_count = dict(sorted(count.items(), key=lambda item: item[1]))
+        sorted_count = dict(sorted(count.items(), key=lambda item: item[1])) #item[1] means frequency , so that when we sort we sort by frequency and not the number itself
 
         return list(sorted_count.keys())[-k:]
             
