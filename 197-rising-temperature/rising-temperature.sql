186f892e970adab3f98d2dde5954e11e01c465ff
Mohammad Jaffer Hussin <mohammad.jaffer.hussin@gmail.com>
2026-08-17 00:32:14 +0530

    Time: 334 ms (10.25%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+SELECT today.id 
+FROM Weather yesterday
+CROSS JOIN Weather today
+WHERE today.recorddate - yesterday.recorddate = 1
+AND today.temperature > yesterday.temperature;
